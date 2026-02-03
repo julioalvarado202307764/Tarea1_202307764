a0476761a31273e5ed238f455de4672c632aff8d
Julio <3708774970101@ingenieria.usac.edu.gt>
2026-02-03 17:43:06 -0600

    subiendo salida de consola

--- tarea1.cpp
+++ tarea1.cpp
@@ -12,23 +12,18 @@ private:
 public:
     int bateria;
 
-    // Métodos tipo "arrow function" (lambdas como propiedades)
     function<void()> cargar;
     function<void(int)> usar;
 
-    // Constructor
     Celular(string m, string mod, int bat) {
         marca = m;
         modelo = mod;
         bateria = bat;
 
-        // Lambda para cargar el celular
         cargar = [this]() {
             bateria = 100;
-            cout << "Celular cargado al 100% 🔋" << endl;
+            cout << "Celular cargado al 100% " << endl;
         };
-
-        // Lambda para usar el celular
         usar = [this](int minutos) {
             bateria -= minutos;
             if (bateria < 0) bateria = 0;
@@ -37,13 +32,10 @@ public:
                  << bateria << "%" << endl;
         };
     }
-
-    // Método tradicional
     void llamar() {
         cout << "Llamando desde el " << marca << " " << modelo << endl;
     }
 
-    // Método tradicional
     void mostrarInfo() {
         cout << "Marca: " << marca 
              << ", Modelo: " << modelo 
@@ -52,11 +44,9 @@ public:
 };
 
 int main() {
-    // Crear dos instancias
     Celular celular1("Samsung", "S23", 80);
     Celular celular2("Apple", "iPhone 14", 60);
 
-    // Probar métodos
     celular1.mostrarInfo();
     celular1.llamar();
     celular1.usar(30);
